4768ec59964fa4d8c8efdbe244a51626135ec29a
Keuller Magalhaes <keuller.magalhaes@gmail.com>
2017-03-21 17:52:49 -0300

    Template fix.

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{msg}}</h1>
+    <h1>\{{msg}}</h1>
     <h2>
         <router-link to="/essential">Essential Links</router-link>
         &nbsp;|&nbsp;
